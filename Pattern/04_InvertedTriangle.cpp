2b7c60428a4d77fabb72f89a1a09c2c7742a799e
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2025-09-04 15:10:35 +0530

    Create 04_InvertedTriangle.cpp

---
+++ Pattern/04_InvertedTriangle.cpp
@@ -0,0 +1,29 @@
+//Question print  :-Inverted Triangle
+// ****
+// ***
+// **
+// *
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int input;
+    cin>>input;
+    
+    for(int i=0; i<input; i++)
+    {
+        for (int j=0; j<input-i; j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    
+    
+    
+    return 0;
+    
+}
