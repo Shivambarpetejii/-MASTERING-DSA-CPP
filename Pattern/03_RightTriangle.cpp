320be779b252fb20094b803007e182d7dc329e5d
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2025-09-04 15:06:06 +0530

    Create 03_RightTriangle.cpp

---
+++ Pattern/03_RightTriangle.cpp
@@ -0,0 +1,28 @@
+//Question print  :- Right Triangle
+// *
+// **
+// ***
+// ****
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int input;
+    cin>>input;
+    
+    for(int i=1; i<=input; i++)
+    {
+        for (int j=0; j<i; j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    
+    
+    
+    return 0;
+    
+}
